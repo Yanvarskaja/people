5747e537d243749a4211d00f55a18e0517ec6f52
Yanvarskaja <88300141+Yanvarskaja@users.noreply.github.com>
2021-08-12 20:58:46 +0800

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,12 @@
 fun main() {
-    val likes = 11
+    val likes = 131
     if (likes == 1) {
         println("Понравилось $likes человеку")
-    } else if (likes >20 && likes < 100 && likes % 10 == 1) {
+    } else if (likes > 20 && likes < 100 && likes % 10 == 1) {
         println("Понравилось $likes человеку")
-    } else if (likes >100 && likes % 100 == 11) {
-        println("Понравилось $likes людям")
+    } else if (likes > 100 && likes % 100 !== 11) {
+        println("Понравилось $likes человеку")
     } else {
-    println("Понравилось $likes людям")
+        println("Понравилось $likes людям")
     }
 }
